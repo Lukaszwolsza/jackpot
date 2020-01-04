19a93b6e5c39e69c5dc2e538e9b191ab1b9f4567
Łukasz Wolsza <luasply@gmail.com>
2020-01-04 16:06:49 +0100

    basic roulette logic

---
+++ assets/scripts/script.js
@@ -0,0 +1,79 @@
+let currentUserBalance = document.getElementById("currentBalance");
+let amountOfWin = document.getElementById("amountOfWins");
+let betOnRed = document.getElementById("color-select-red");
+let betOnBlack = document.getElementById("color-select-black");
+let betOnGreen = document.getElementById("color-select-green");
+let winnerColor = document.getElementById("color-winner");
+const btnToSend = document.getElementById("apply-your-choice");
+let colorWinner = document.getElementById("color-winner");
+let secondTimer = document.getElementById("seconds");
+
+//======================RANDOM NUMBERS===========================//
+function randomColorWinner(){
+    let resultOfRandom = Math.floor((Math.random() * 100) + 1);
+
+        if(resultOfRandom > 0 && resultOfRandom < 49){
+            colorWinner.style.color = "red";
+            colorWinner.textContent = "red";    
+        } else if (resultOfRandom > 50 && resultOfRandom <= 100){
+            colorWinner.style.color = "black";
+            colorWinner.textContent = "black"; 
+        } else if (resultOfRandom === 49){
+            colorWinner.style.color = "green";
+            colorWinner.textContent = "green"; 
+        }     
+}
+
+//==============================================================//
+let seconds = 30;
+let timerToNewGame;
+function timerToStartGame(){
+    if(seconds > 0)
+    {
+        seconds--;
+        secondTimer.innerHTML = seconds; 
+        
+    }else {
+        seconds = 31;
+
+        randomColorWinner();
+    }
+    
+}
+
+ timerToNewGame = setInterval(timerToStartGame, 1000);
+
+
+
+
+
+
+let defaultAmount = 0;
+let currentBalance = defaultAmount;
+
+// function getCurrentAmount(){
+//     return parseInt(currentUserBalance.value);
+// }
+
+function getUserInputOnRed(){
+    return parseInt(betOnRed.value);
+}
+function getUserInputOnBlack(){
+    return parseInt(betOnBlack.value);
+}
+function getUserInputOnRed(){
+    return parseInt(betOnGreen.value);
+}
+
+function writeAndOuput(value){
+    currentUserBalance.textContent = value;
+}
+
+function makeBet(){
+    const userBet = getUserInputOnRed();
+    const userBalance = currentBalance;
+    currentBalance -= userBet;
+    writeAndOuput(userBalance);
+}
+
+btnToSend.addEventListener("click", makeBet);
