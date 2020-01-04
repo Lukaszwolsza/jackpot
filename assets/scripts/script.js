4d9cb8a270aa46778c577d98c6ebba6f52398f48
unknown <luasply@gmail.com>
2020-01-04 22:55:15 +0100

    add timer with boxes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,46 +7,59 @@ let winnerColor = document.getElementById("color-winner");
 const btnToSend = document.getElementById("apply-your-choice");
 let colorWinner = document.getElementById("color-winner");
 let secondTimer = document.getElementById("seconds");
+let colorBox = document.getElementById("color-box");
+
+let counter = 0;
+
+function settingColorOfBox(color){
+    
+    for(let i = counter; i <= 20; i++){
+        if(color === "red"){
+            document.getElementById("color-box"+i).style.backgroundColor = "red";
+        } else if ( color === "black"){
+            document.getElementById("color-box"+i).style.backgroundColor = "black";
+        } else if ( color === "green"){
+            document.getElementById("color-box"+i).style.backgroundColor = "green";
+        }
+    }
+    counter++;
+}
 
 //======================RANDOM NUMBERS===========================//
 function randomColorWinner(){
     let resultOfRandom = Math.floor((Math.random() * 100) + 1);
-
+        counter++;
         if(resultOfRandom > 0 && resultOfRandom < 49){
             colorWinner.style.color = "red";
             colorWinner.textContent = "red";    
+            settingColorOfBox("red");
         } else if (resultOfRandom > 50 && resultOfRandom <= 100){
             colorWinner.style.color = "black";
             colorWinner.textContent = "black"; 
+            settingColorOfBox("black");
         } else if (resultOfRandom === 49){
             colorWinner.style.color = "green";
             colorWinner.textContent = "green"; 
+            settingColorOfBox("green");
         }     
+        
 }
-
 //==============================================================//
-let seconds = 30;
+
+let seconds = 3;
 let timerToNewGame;
 function timerToStartGame(){
     if(seconds > 0)
     {
         seconds--;
-        secondTimer.innerHTML = seconds; 
-        
+        secondTimer.innerHTML = seconds;     
     }else {
-        seconds = 31;
-
+        seconds = 3;
         randomColorWinner();
-    }
-    
+    }  
 }
 
- timerToNewGame = setInterval(timerToStartGame, 1000);
-
-
-
-
-
+timerToNewGame = setInterval(timerToStartGame, 1000);
 
 let defaultAmount = 0;
 let currentBalance = defaultAmount;
