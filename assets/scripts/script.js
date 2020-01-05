0abdaf9f2e4af721674320b3e3b2884c88caec73
Łukasz Wolsza <luasply@gmail.com>
2020-01-05 20:41:13 +0100

    fixed colors of boxes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,30 +10,12 @@ let secondTimer = document.getElementById("seconds");
 let colorBox = document.getElementById("color-box");
 
 let counter = 1;
-
-// function settingColorOfBox(color){
-    
-//     for(let i = counter; i <= 20; i++){
-//         if(color === "red"){
-//             document.getElementById("color-box"+i).style.backgroundColor = "red";
-//         } else if ( color === "black"){
-//             document.getElementById("color-box"+i).style.backgroundColor = "black";
-//         } else if ( color === "green"){
-//             document.getElementById("color-box"+i).style.backgroundColor = "green";
-//         }
-//     }
-//     counter++;
-// }
+let seconds = 1;
+let timerToNewGame;
 
 //======================RANDOM NUMBERS===========================//
 function randomColorWinner(){
     let resultOfRandom = Math.floor((Math.random() * 100) + 1);
-    if(counter === 0){
-        counter = 0;
-        for(let i = 1; i < 18; i++){
-            document.getElementById("color-box"+i).style.backgroundColor = transparent;
-        }
-    } else {
         if(resultOfRandom > 0 && resultOfRandom < 49){
             colorWinner.style.color = "red";
             colorWinner.textContent = "red";    
@@ -46,34 +28,27 @@ function randomColorWinner(){
             colorWinner.style.color = "green";
             colorWinner.textContent = "green"; 
             document.getElementById("color-box"+counter).style.backgroundColor = "green";
-        }     
-        
-    }  
-    counter++; 
-    
+        }         
+    counter++;
+    if(counter === 18){
+        for(let i = 1; i <= 19; i++){
+            document.getElementById("color-box"+i).style.backgroundColor = "transparent";
+        }
+        counter = 1;    
+    }
 }
 //==============================================================//
 
-let seconds = 1;
-let timerToNewGame;
 function timerToStartGame(){
     if(seconds > 0){
         seconds--;
         secondTimer.innerHTML = seconds;     
     }else {
-        seconds = 1;
+        seconds = 31;
         randomColorWinner();
     }  
 }
-
 timerToNewGame = setInterval(timerToStartGame, 1000);
-
-let defaultAmount = 0;
-let currentBalance = defaultAmount;
-
-// function getCurrentAmount(){
-//     return parseInt(currentUserBalance.value);
-// }
 
 function getUserInputOnRed(){
     return parseInt(betOnRed.value);
@@ -81,19 +56,19 @@ function getUserInputOnRed(){
 function getUserInputOnBlack(){
     return parseInt(betOnBlack.value);
 }
-function getUserInputOnRed(){
+function getUserInputOnGreen(){
     return parseInt(betOnGreen.value);
 }
-
 function writeAndOuput(value){
     currentUserBalance.textContent = value;
 }
-
 function makeBet(){
     const userBet = getUserInputOnRed();
-    const userBalance = currentBalance;
-    currentBalance -= userBet;
-    writeAndOuput(userBalance);
+    console.log(userBet);
+   
+    currentUserBalance += userBet;
+    console.log(currentUserBalance);
+    const currentValueOfPocket = currentUserBalance;
+    writeAndOuput(currentValueOfPocket);
 }
-
 btnToSend.addEventListener("click", makeBet);
